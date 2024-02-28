1e3dac06f57c959e65e28bc4aa6ef1ba85d2a8f1
ViditRajpurohit <viditrajpurohit27@gmail.com>
2024-02-29 05:12:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: {
+    colors: {
+      sportGreen : "#95c11e",
+    },
+     },
+  },
+  plugins: [],
+}
+
